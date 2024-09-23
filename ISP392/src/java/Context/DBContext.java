3837b911f70a8d3e6f987025240de9bfd6dc6a05
khucxuanhoa <khucxuanhoa111@gmail.com>
2024-09-23 20:08:16 +0700

    Tạo class cho entity

---
+++ ISP392/src/java/Context/DBContext.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DBContext;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBContext {
+
+    protected Connection connection;
+
+    public DBContext() {
+        try {
+            // Edit URL , username, password to authenticate with your MS SQL Server
+            String port = "2811";
+            String DB = "";
+            String url = "jdbc:sqlserver://localhost:" + port + "2811;databaseName=" + DB;
+            String username = "sa";
+            String password = "123456";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+}
